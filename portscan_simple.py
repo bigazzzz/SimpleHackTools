2b7c02a33949c93ea25bf15710ec831925924c6a
biga <noemail@email.no>
2019-10-16 01:35:28 +0300

    add simple port scanner

--- portscan_simple.py
+++ portscan_simple.py
@@ -1,16 +1,15 @@
 # -*- coding:utf -8 -*-
 import socket
 
-ports = [20, 21, 22, 23, 42, 43, 53, 67, 69, 80]
 host = input("Хост => ")
+port = int(input("Порт => "))
+
 print("\nLet\'s portscan begin")
 
-for port in ports:
-    try:
-        scan = socket.socket()
-        scan.settimeout(0.5)
-        scan.connect((host, port))
-    except socket.error:
-        print("- Порт {0} закрыт".format(port))
-    else:
-        print("+ Порт {0} открыт".format(port))
+try:
+    scan = socket.socket()
+    scan.connect((host, port))
+except socket.error:
+    print("- Порт {0} закрыт".format(port))
+else:
+    print("+ Порт {0} открыт".format(port))
