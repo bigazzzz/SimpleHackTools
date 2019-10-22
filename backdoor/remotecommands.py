c319e4f5be463d0f8b3beb2813e0b295637171ea
biga <noemail@email.no>
2019-10-23 01:22:38 +0300

    added pythoninfo

--- backdoor/remotecommands.py
+++ backdoor/remotecommands.py
@@ -2,17 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import inspect
+import sys
 
 class RemoteCommands:
     '''
     Модуль с командами для клиентского backdoor
     '''
-
-    def sysinfo():
-        '''
-        Вывод строчки с информацией об ОС
-        '''
-        return 'Информация о системе'
 
     def help():
         '''
@@ -24,6 +19,19 @@ class RemoteCommands:
         for f in functions:
             result += f[0] + '\n\t' + f[1].__doc__.strip() + '\n'
         return result
+
+    def sysinfo():
+        '''
+        Вывод строчки с информацией об ОС
+        '''
+        return 'Информация о системе'
+
+    def pythoninfo():
+        '''
+        Вывод строчки с информацией об установленной версии python
+        '''
+        return(sys.version)
+
 
 if __name__ == "__main__":
     print(RemoteCommands.help())
