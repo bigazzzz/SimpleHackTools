d558aff491a9473fbcb1c861618a3b5a3d291df4
biga <noemail@email.no>
2019-10-23 00:35:01 +0300

    Revert "неработающий код"
    This reverts commit 7a5af2ed2778f664a55913ff32992577d9cc5c8f.

--- backdoor/client_back_command.py
+++ backdoor/client_back_command.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import remotecommands as command
 
 sock = socket.socket()
 sock.bind(('', 4444))
@@ -13,16 +12,10 @@ conn, addr = sock.accept()
 print ('connected:', addr)
 
 while True:
-    data = conn.recv(1024).decode('utf-8')
-
-    if data == 'exit':
+    data = conn.recv(1024)
+    print(data.decode('utf-8'))
+    if data == b'exit':
         break
- #   try:
-    func = getattr(command,data)
-    data = func()
-    #except:
-    #    data = 'Такой команды нет. Наберите help для справки'
-    conn.send(bytes(data, encoding='utf-8'))
+    conn.send(data.upper())
 
-conn.send(bytes("Работа завершена", encoding='utf-8'))
 conn.close()
