cb6b7a5048efb002ceb26ef9e29b400fe96ec724
bigazzzz <bigazzzz@gmail.com>
2019-11-05 17:59:43 +0300

    find hrefs in html page

--- spider/spider.py
+++ spider/spider.py
@@ -2,10 +2,27 @@ import requests
 import re
 
 def emails(text):
-    pattern = '[A-Za-z0-9+%-.]+@[A-Za-z0-9-.]+\.[A-Za-z]+'
+    pattern = r'[A-Za-z0-9+%-.]+@[A-Za-z0-9-.]+\.[A-Za-z]+'
     return set(re.findall(pattern, text))
+
+def links(text):
+    bad_result = set()
+    pattern = r'href="(.+)"'
+    bad_urls = [
+        'mailto:(.+)',
+        'tel:(.+)',
+        'javascript:(.*)',
+        '#(.*)'
+    ]
+    urls = set(re.findall(pattern, text))
+    for url in urls:
+        for bad_url in bad_urls:
+            if re.fullmatch(bad_url, url) != None:
+                bad_result.add(url)
+    return urls - bad_result
+
 
 url = 'http://evil.bigazzzz.ru:15073/test.html'
 html_response = requests.request('GET', url)
 ''' в html_response.text - текст html-документа'''
-print(emails(html_response.text))
+print(links(html_response.text))
