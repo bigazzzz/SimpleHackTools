18abc9b38b2b62f2adb9eb763d21e3c925b6afca
bigazzzz <bigazzzz@gmail.com>
2019-11-15 14:01:01 +0300

    return json

--- spider/spider.py
+++ spider/spider.py
@@ -1,9 +1,10 @@
 import requests
 import re
+import json
 
 def emails(text):
     pattern = r'[A-Za-z0-9+%-.]+@[A-Za-z0-9-.]+\.[A-Za-z]+'
-    return set(re.findall(pattern, text))
+    return json.dumps(list(set(re.findall(pattern, text))))
 
 def links(text):
     bad_result = set()
@@ -19,11 +20,22 @@ def links(text):
         for bad_url in bad_urls:
             if re.fullmatch(bad_url, url) != None:
                 bad_result.add(url)
-    return urls - bad_result
+    return json.dumps(list(urls - bad_result))
 
 def forms(text):
-    pattern = r'<form\s(.+?)>(.*?)</form>'
-    return re.findall(pattern,text,re.DOTALL)
+    form_attr = ('name', 'method', 'action')
+    forms = dict()
+    pattern = r'<form\s(.+?)>.*?</form>'
+    for form_id, form in enumerate(re.findall(pattern, text, re.DOTALL)):
+        forms[form_id] = dict()
+        for attr in form_attr:
+            match = re.search(attr + r'=["\'](.*?)["\']', form)
+            if match:
+                forms[form_id][attr] = match.group(1)
+    pattern = r'<form\s.+?>(.*?)</form>'
+    for form_id, form in enumerate(re.findall(pattern, text, re.DOTALL)):
+        forms[form_id]['content'] = form
+    return json.dumps(forms)
 
 
 def hr():
